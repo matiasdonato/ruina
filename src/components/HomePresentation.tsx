4871dec4f067891e62a2a4015fce7dc28e94476e
matiasdonato <matiasdonato14@gmail.com>
2024-09-02 01:57:22 -0300

    fix autoplay

--- src/components/HomePresentation.tsx
+++ src/components/HomePresentation.tsx
@@ -2,7 +2,7 @@ import "../css/HomePresentation.css";
 import arrow from "../assets/button-down.png";
 //@ts-ignore
 import crowVideo from "../assets/home.mp4"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function HomePresentation() {
   const [showButton, setShowButton] = useState(true)
@@ -15,9 +15,27 @@ export default function HomePresentation() {
     setShowButton(false)
   };
 
+  useEffect(() => {
+    const myVideo = document.getElementById('myVideo') as HTMLVideoElement;
+    if (myVideo !== null) {
+      const playPromise = myVideo.play();
+    
+      if (playPromise !== undefined) {
+        playPromise.then(() => {
+          // El video comenzó a reproducirse
+        }).catch(() => {
+          // Si la reproducción automática falla, intenta reproducir tras una interacción del usuario
+          document.body.addEventListener('click', () => {
+            myVideo.play();
+          }, { once: true });
+        });
+      }
+    }
+  }, []);
+
   return (
     <div className="videoBox">
-      <video src={crowVideo} autoPlay muted loop></video>
+      <video id="myVideo" src={crowVideo} autoPlay muted loop></video>
       <div className="layerContainer">
         <h1 unselectable="on" className="animate__animated animate__fadeIn" style={{animation: 'fadeIn 5.5s'}} >RUINA</h1>
       </div>
